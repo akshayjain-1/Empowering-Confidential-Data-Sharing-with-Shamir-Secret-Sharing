c2879e66ecfc2d9735cabd35253acb3309305051
Akshay Jain <135832006+akshayjain-1@users.noreply.github.com>
2024-04-05 22:50:56 -0400

    Update new.py

--- new.py
+++ new.py
@@ -9,7 +9,7 @@ SCOPES=['https://www.googleapis.com/auth/drive']
 
 service= Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-folder_id='1VPkA0Qf89Rcd-Jf10bwJ3h5fO7755712'
+folder_id='<Folder id for your Google drive folder>'
 
 file_names=[input("\nEnter filename to write (Enter extenxion): ")]
 mime_types=['text/plain']
@@ -21,7 +21,7 @@ for file_name, mime_type in zip(file_names,mime_types):
 
     }
 
-    media= MediaFileUpload('/home/akshay/Desktop/Data app/New/{0}'.format(file_name), mimetype=mime_type)
+    media= MediaFileUpload('<path where the file to be uploaded is stored>'.format(file_name), mimetype=mime_type)
 
     service.files().create(
         body=file_metadata,
